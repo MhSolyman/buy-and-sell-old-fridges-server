7fdd6668033518ac78efadce7d6dbf087b45ad39
MhSolyman <solymanbijoye.com>
2022-11-27 23:17:03 +0600

    add  usr route

--- index.js
+++ index.js
@@ -47,6 +47,13 @@ const run = async () => {
             res.send(result)
 
         })
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            console.log(email)
+            const query = {email};
+            const result = await userCullection.findOne(query)
+            res.send(result)
+        })
 
 
     }
