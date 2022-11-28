3103aa8d9d522e50e22f5092c41ab56837767927
MhSolyman <solymanbijoye.com>
2022-11-29 00:48:05 +0600

    add user router

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express()
-const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
-const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+const { query } = require('express');
 const port = process.env.PORT || 5000
 
 
@@ -32,32 +33,55 @@ const run = async () => {
 
         app.post('/product', async (req, res) => {
             const user = req.body;
+            const name = user.name;
+            const query = { name: name }
+            console.log(name)
             const result = await productsCollection.insertOne(user);
             res.send(result)
+            
+            
+            const categories = await categoriesCollection.findOne(query)
+            console.log(categories)
+            if (categories === null) {
+                const categoriesTwo = await categoriesCollection.insertOne(query);
+              console.log("nai")
+
+            } else{
+                console.log('asa')
+                
+            }
 
         })
 
-        app.get('/category/:id', async(req, res) => {
+        app.get('/category/:id', async (req, res) => {
             const id = req.params.id;
             console.log(id)
-            const query = {_id: ObjectId(id)}
+            const query = { _id: ObjectId(id) }
             const result = await categoriesCollection.findOne(query)
-            const nameRe=result.name;
-            const queryTwo= {name:nameRe}
+            const nameRe = result.name;
+            const queryTwo = { name: nameRe }
             const resultTwo = await productsCollection.find(queryTwo).toArray()
             res.send(resultTwo)
 
         })
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await userCullection.insertOne(user);
             res.send(result)
 
         })
+        app.get('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query ={_id:ObjectId(id)}
+            const result = await userCullection.findOne(query);
+            res.send(result)
+
+        })
         app.get('/users/:email', async (req, res) => {
             const email = req.params.email;
-         
-            const query = {email};
+
+            const query = { email };
             const result = await userCullection.findOne(query)
             res.send(result)
         })
